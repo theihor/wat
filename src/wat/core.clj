7ac95fd56c5ff6993f3c21a5a60b4fdab6709363
rybiyb <bogdan.rybiy@hengsoft.net>
2015-07-02 19:33:04 +0300

    Having fun with ring

--- src/wat/core.clj
+++ src/wat/core.clj
@@ -1,6 +1,35 @@
 (ns wat.core)
 
+(use 'ring.adapter.jetty)
+(use 'ring.util.response)
+(use 'ring.middleware.resource)
+
 (defn handler [request]
-  {:status 200
-   :headers {"Content-Type" "text/html"}
-   :body "Hello World"})
+  (let [url (:uri request)]
+    (println url)
+   (cond
+     (= "/lol" url)
+       (redirect "https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+     (= "/dummy" url) {:status 200
+            :headers {"Content-Type" "text/html"}
+            :body "Welcome on dummy page!"}
+     :else {:status 200
+            :headers {"Content-Type" "text/html"}
+            :body (str "<script>
+                        function ahtung(){
+                            alert (\"Shit happened!\")
+                        }
+                        </script>
+                        <center>
+                          <a href=\"/dummy\"> <img src=\"favicon.ico\" onclick=\"ahtung()\" > </a> <br>
+                        Click me! 
+                        </center>
+                        "
+                        ) }
+     )))
+
+(def app
+  (-> #'handler
+   (wrap-resource "public")))
+
+(defonce server (ring.adapter.jetty/run-jetty #'app {:port 3030 :join? false}))
